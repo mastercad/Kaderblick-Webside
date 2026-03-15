21e69ad71338bdce08946cc90ebcaf994c0421f5
Andreas Kempe <andreas.kempe@byte-artist.de>
2026-03-15 19:18:48 +0100

    codestyles adjusted

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use DateTime;
+use Deprecated;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -879,7 +880,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    #[\Deprecated]
+    #[Deprecated]
     public function eraseCredentials(): void
     {
     }
