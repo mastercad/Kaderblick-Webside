db2be69fc40ede0fccecfcfdd3ff74aa3c188c21
Andreas Kempe <andreas.kempe@byte-artist.de>
2026-05-02 21:46:12 +0200

    fix: prevent  create cron_heartbeat table with migration failing on old db dump

--- api/migrations/Version20260430223017.php
+++ api/migrations/Version20260430223017.php
@@ -16,7 +16,7 @@ final class Version20260430223017 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("CREATE TABLE cron_heartbeats (id INT AUTO_INCREMENT NOT NULL, command VARCHAR(255) NOT NULL, last_run_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', last_error LONGTEXT DEFAULT NULL, running_pid INT DEFAULT NULL, running_started_at INT DEFAULT NULL, UNIQUE INDEX UNIQ_8BB672EA8ECAEAD4 (command), INDEX idx_cron_heartbeats_command (command), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB");
+        $this->addSql("CREATE TABLE IF NOT EXISTS cron_heartbeats (id INT AUTO_INCREMENT NOT NULL, command VARCHAR(255) NOT NULL, last_run_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', last_error LONGTEXT DEFAULT NULL, running_pid INT DEFAULT NULL, running_started_at INT DEFAULT NULL, UNIQUE INDEX UNIQ_8BB672EA8ECAEAD4 (command), INDEX idx_cron_heartbeats_command (command), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB");
     }
 
     public function down(Schema $schema): void
